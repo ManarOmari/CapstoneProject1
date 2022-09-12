f06c17d18096b8a943be2c634db56ebb823bd640
Manar <manar.qasem732016@gmail.com>
2022-09-12 17:49:52 +0300

    update sport.js

--- sport.js
+++ sport.js
@@ -4,32 +4,32 @@ row.className = 'row';
 
 const sport = [
   {
-    img: '/assets/images/walking2.jpg',
+    img: 'assets/images/walking2.jpg',
     title: 'Walking',
     description: 'You carry your own body weight when you walk. This is known as weight-bearing exercise. ',
   },
   {
-    img: '/assets/images/jumping.jpg',
+    img: 'assets/images/jumping.jpg',
     title: 'Jumping Jack',
     description: 'In Jumping Jacks, you bring your arms and legs together or apart at the same time while jumping up in the air.',
   },
   {
-    img: '/assets/images/push.jpg',
+    img: 'assets/images/push.jpg',
     title: 'Push-ups',
     description: 'Put your hands parallel and shoulder-width apart. Keep your back straight and your neck as unstrained as possible. ',
   },
   {
-    img: '/assets/images/squat.jpg',
+    img: 'assets/images/squat.jpg',
     title: 'Squats',
     description: 'Position your feet hip-wide. Crouch down. Do not push your knees forward. ',
   },
   {
-    img: '/assets/images/plank.jpg',
+    img: 'assets/images/plank.jpg',
     title: 'Planks',
     description: 'You lie on your stomach. Now lean on your forearms. Your elbows are at shoulder level and your forearms are parallel.',
   },
   {
-    img: '/assets/images/superman.jpg',
+    img: 'assets/images/superman.jpg',
     title: 'Superman',
     description: 'Lie on your stomach. The legs are parallel. Stretch out your arms above your head and put them down. ',
   },
