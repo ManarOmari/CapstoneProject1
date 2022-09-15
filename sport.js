1bb2ec8ed0f465c58af519b986c2a58593dde39f
Manar <manar.qasem732016@gmail.com>
2022-09-15 09:42:55 +0300

    Update about Secrtion mobile

--- sport.js
+++ sport.js
@@ -1,38 +1,39 @@
-const sports = document.querySelector('.sports');
-const main1 = document.querySelector('.main1');
-const row = document.createElement('div');
-row.className = 'row';
+const sports = document.querySelector(".sports");
+const main1 = document.querySelector(".main1");
+const row = document.createElement("div");
+row.className = "row";
 
 const sport = [
+  
   {
-    img: 'assets/images/walking2.jpg',
-    title: 'Walking',
+    img: "assets/images/walking2.jpg",
+    title: "Walking",
     description:
-      'You carry your own body weight when you walk. This is known as weight-bearing exercise. ',
+      "You carry your own body weight when you walk. This is known as weight-bearing exercise. ",
   },
   {
-    img: 'assets/images/jumping.jpg',
-    title: 'Jumping Jack',
+    img: "assets/images/jumping.jpg",
+    title: "Jumping Jack",
     description:
-      'In Jumping Jacks, you bring your arms and legs together or apart at the same time while jumping up in the air.',
+      "In Jumping Jacks, you bring your arms and legs together or apart at the same time while jumping up in the air.",
   },
   {
-    img: 'assets/images/push.jpg',
-    title: 'Push-ups',
+    img: "assets/images/squat.jpg",
+    title: "Squats",
     description:
-      'Put your hands parallel and shoulder-width apart. Keep your back straight and your neck as unstrained as possible. ',
+      "Position your feet hip-wide. Crouch down. Do not push your knees forward. ",
   },
   {
-    img: 'assets/images/squat.jpg',
-    title: 'Squats',
+    img: "assets/images/plank.jpg",
+    title: "Planks",
     description:
-      'Position your feet hip-wide. Crouch down. Do not push your knees forward. ',
+      "You lie on your stomach. Now lean on your forearms. Your elbows are at shoulder level and your forearms are parallel.",
   },
   {
-    img: 'assets/images/plank.jpg',
-    title: 'Planks',
+    img: "assets/images/push.jpg",
+    title: "Push-ups",
     description:
-      'You lie on your stomach. Now lean on your forearms. Your elbows are at shoulder level and your forearms are parallel.',
+      "Put your hands parallel and shoulder-width apart. Keep your back straight and your neck as unstrained as possible. ",
   },
   {
     img: 'assets/images/super.jpg',
@@ -40,36 +41,36 @@ const sport = [
     description:
       'Lie on your stomach. The legs are parallel. Stretch out your arms above your head and put them down. ',
   },
+  
 ];
 function createCard() {
   for (let i = 0; i < sport.length; i += 1) {
-    const col = document.createElement('div');
-    col.className = 'col-sm-6';
-    const card = document.createElement('div');
-    card.className = 'card1';
+    const col = document.createElement("div");
+    col.className = "col-sm-6";
+    const card = document.createElement("div");
+    card.className = "card1";
     const image = new Image();
     image.src = sport[i].img;
-    const div2 = document.createElement('div');
-    div2.className = 'card2';
-    const h = document.createElement('h3');
-    h.innerHTML = sport[i].title;
-    const redTxt = document.createElement('p');
-    redTxt.classList.add('redTxt');
-    redTxt.innerHTML = 'Ecsersise ';
-    const horizantalLine = document.createElement('hr');
-    horizantalLine.className = 'horizantalLine';
-    const desc = document.createElement('p');
-    desc.innerHTML = sport[i].description;
-    div2.appendChild(h);
-    div2.appendChild(redTxt);
-    div2.appendChild(horizantalLine);
-    div2.appendChild(desc);
     card.appendChild(image);
-    card.appendChild(div2);
     col.appendChild(card);
+    const div2 = document.createElement("div");
+    div2.className = "card2";
+    col.appendChild(div2);
+    const h = document.createElement("h3");
+    h.innerHTML = sport[i].title;
+    div2.appendChild(h);
+    const redTxt = document.createElement("p");
+    redTxt.classList.add("redTxt");
+    redTxt.innerHTML = "Ecsersise ";
+    div2.appendChild(redTxt);
+    const horizantalLine = document.createElement("hr");
+    horizantalLine.className = "horizantalLine";
+    div2.appendChild(horizantalLine);
+    const desc = document.createElement("p");
+    desc.innerHTML = sport[i].description;
+    div2.appendChild(desc);
     row.appendChild(col);
   }
-
   sports.appendChild(row);
   main1.append(sports);
 }
